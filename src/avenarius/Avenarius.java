ce73513b3150b551608f9ebda0287beef17b0bde
wdmavenarius@gmail.com <>
2018-10-22 15:38:32 -0400

    Hello, this is a comment

---
+++ src/avenarius/Avenarius.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avenarius;
+
+/**
+ *
+ * @author wa2
+ */
+public class Avenarius {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Avenarius();
+    }
+    public static void Avenarius()
+    {
+        System.out.println("Willem Avenarius");
+    }
+    
+}
